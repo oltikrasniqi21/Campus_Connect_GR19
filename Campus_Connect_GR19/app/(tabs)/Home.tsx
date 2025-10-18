a301c4d0cb4a77fc9cbe7edd6013dbf1d871c716
oltikrasniqi21 <oltkrasn@gmail.com>
2025-10-19 00:14:53 +0200

    test

---
+++ Campus_Connect_GR19/app/(tabs)/Home.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet,} from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View,} from '@/components/Themed';
+import { Link } from 'expo-router';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },    
+
+});
