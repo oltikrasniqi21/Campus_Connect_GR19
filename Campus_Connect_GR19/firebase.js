d348ff8485afb457e6b9656268a5b50c7423c879
Valmir Mustafa <valmirmustafa2005@gmail.com>
2025-11-16 15:28:02 +0100

    fixed web errors

--- Campus_Connect_GR19/firebase.js
+++ Campus_Connect_GR19/firebase.js
@@ -1,29 +1,32 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { Platform } from "react-native";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC0YCZXzpkjrE1zEATmz4KLW0o0_Gf-anM",
   authDomain: "campus-connect-gr19.firebaseapp.com",
   projectId: "campus-connect-gr19",
   storageBucket: "campus-connect-gr19.firebasestorage.app",
   messagingSenderId: "794887817202",
-  appId: "1:794887817202:web:0d06f8cf3bae7591738d6b"
+  appId: "1:794887817202:web:0d06f8cf3bae7591738d6b",
 };
+
 const app = initializeApp(firebaseConfig);
 
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-});
-
+const auth =
+  Platform.OS === "web"
+    ? initializeAuth(app, { persistence: browserLocalPersistence })
+    : initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage),
+      });
 
 const db = getFirestore(app);
 
 export { auth, db };
 export default app;
-
